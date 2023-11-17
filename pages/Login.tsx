4a7027c7b0945b1077de64f0d831f3bb6cc1389e
irvanshandika <shandikamuhammadirvan@gmail>
2023-11-17 10:53:44 +0700

    Add amplify.yml and change script code

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,17 +1,131 @@
-function Login() {
+import React, { useState, FormEvent, ChangeEvent } from "react";
+
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    // Add your login logic here
+  };
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-white rounded-lg shadow-lg p-8">
-        <h2 className="text-xl font-bold mb-4">This page is under development</h2>
-        <p className="text-gray-600">We're sorry, but this page is not yet ready for use. Please check back later.</p>
-        <div className="mt-4">
-          <a href="/">
-            <button className="bg-red-500 hover:bg-red-400 px-4 py-2 text-white rounded-lg">Back</button>
-          </a>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <div className="text-center">
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Welcome Back!</h2>
+            <p className="mt-2 text-center text-sm text-gray-600">Please sign in to your account</p>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  value={email}
+                  onChange={handleEmailChange}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={handlePasswordChange}
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fillRule="evenodd" d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                    <path fillRule="evenodd" d="M3 10a7 7 0 1114 0 7 7 0 01-14 0zm7-6a1 1 0 00-1 1v3a1 1 0 102 0V5a1 1 0 00-1-1zm0 10a1 1 0 100 2 1 1 0 000-2z" />
+                  </svg>
+                </span>
+                Sign in
+              </button>
+            </div>
+            <div className="mt-4 text-sm text-center">
+              <span className="font-medium text-gray-600">Or sign in with</span>
+            </div>
+            <div className="mt-4 flex justify-center space-x-4">
+              <button
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 0C5.373 0 0 5.373 0 12C0 18.627 5.373 24 12 24C18.627 24 24 18.627 24 12C24 5.373 18.627 0 12 0ZM17.646 10.353L12.707 10.353L12.707 7.293C12.707 6.779 12.309 6.353 11.795 6.353C11.281 6.353 10.883 6.779 10.883 7.293L10.883 10.353L6.354 10.353C5.84 10.353 5.414 10.749 5.414 11.263C5.414 11.777 5.84 12.202 6.354 12.202L10.883 12.202L10.883 15.263C10.883 15.777 11.281 16.202 11.795 16.202C12.309 16.202 12.707 15.777 12.707 15.263L12.707 12.202L17.646 12.202C18.16 12.202 18.586 11.777 18.586 11.263C18.586 10.749 18.16 10.353 17.646 10.353Z"></path>
+                </svg>
+                Google
+              </button>
+              <button
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 0C5.373 0 0 5.373 0 12C0 18.627 5.373 24 12 24C18.627 24 24 18.627 24 12C24 5.373 18.627 0 12 0ZM16.5 12.75L12.75 12.75L12.75 16.5C12.75 16.914 12.414 17.25 12 17.25C11.586 17.25 11.25 16.914 11.25 16.5L11.25 12.75L7.5 12.75C7.086 12.75 6.75 12.414 6.75 12C6.75 11.586 7.086 11.25 7.5 11.25L11.25 11.25L11.25 7.5C11.25 7.086 11.586 6.75 12 6.75C12.414 6.75 12.75 7.086 12.75 7.5L12.75 11.25L16.5 11.25C16.914 11.25 17.25 11.586 17.25 12C17.25 12.414 16.914 12.75 16.5 12.75Z"></path>
+                </svg>
+                Apple
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
