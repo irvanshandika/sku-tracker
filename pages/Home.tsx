8bba04ce9d935649d61d8ac17f5ed8f4558df28c
irvanshandika <shandikamuhammadirvan@gmail>
2023-11-18 17:46:20 +0700

    Trying add animation

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+
 function Home() {
   return (
     <>
@@ -6,6 +8,7 @@ function Home() {
           <h1 className="text-4xl font-bold text-center text-gray-800">
             Selamat Datang Di S<span>K</span>U! Tracker
           </h1>
+          <Player autoplay loop src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json" style={{ height: "300px", width: "300px" }}></Player>
           <p className="mt-4 text-lg text-center text-gray-600">This template comes pre-configured with Tailwind CSS and is ready to use with one command!</p>
           <a href="#" className="mt-8 px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600">
             Get Started
