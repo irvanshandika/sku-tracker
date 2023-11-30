330b74e23467e641c4173a7ea32bdbb6dd244152
irvanshandika <shandikamuhammadirvan@gmail>
2023-11-30 23:56:56 +0700

    Change pnpm to npm, because have some problem

--- app/main.tsx
+++ app/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
-import "swiper"
-import "swiper/css"
+import "swiper";
+import "swiper/css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent, ChangeEvent } from "react";
+import { IonIcon } from "@ionic/react";
 
 interface LoginProps {}
 
@@ -85,11 +86,8 @@ const Login: React.FC<LoginProps> = () => {
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                    <path fillRule="evenodd" d="M3 10a7 7 0 1114 0 7 7 0 01-14 0zm7-6a1 1 0 00-1 1v3a1 1 0 102 0V5a1 1 0 00-1-1zm0 10a1 1 0 100 2 1 1 0 000-2z" />
-                  </svg>
+                <span className="absolute left-0 inset-y-0 flex items-center pl-32">
+                  <IonIcon name="log-in" className="w-5 h-5"></IonIcon>
                 </span>
                 Sign in
               </button>
@@ -100,6 +98,7 @@ const Login: React.FC<LoginProps> = () => {
             <div className="mt-4 flex justify-center space-x-4">
               <button
                 type="button"
+                disabled
                 className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                 <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path
@@ -111,6 +110,7 @@ const Login: React.FC<LoginProps> = () => {
               </button>
               <button
                 type="button"
+                disabled
                 className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
                 <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path
